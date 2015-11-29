4adc09d59a8e318aa92d787f34b94a823c16ebc4
Adam Melkus <adam@melkus.info>
2015-11-29 21:38:52 +0100

    fixed tag counter cache fixed tag uniqueness

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,7 @@ class Post < ActiveRecord::Base
 
   attr_accessor :post_tags_string
 
-  before_save :parse_tags
+  before_save :parse_and_save_tags
   before_destroy :decrement_post_counter
   after_commit { Tag.clean_unassociated }
 
@@ -32,14 +32,24 @@ class Post < ActiveRecord::Base
     tags.each { |t| t.decrement!(:post_count) }
   end
 
-  def parse_tags
-    self.tags = []
-    my_tags = post_tags_string.split(/[\s,.]+/).map(&:strip)
-    my_tags.each do |name|
+  def parse_and_save_tags
+    my_tags = post_tags_string.split(/[\s,.]+/).map(&:strip).uniq
+    names = tags.map(&:name)
+    to_add = my_tags - names
+    to_remove = names - my_tags
+
+    to_add.each do |name|
       t = Tag.find_or_create_by(name: name)
       t.increment(:post_count)
       t.save
-      tags << t unless tags.include?(t)
+      tags << t
     end
+
+    to_remove.each do |name|
+      t = Tag.find_by_name(name)
+      t.decrement!(:post_count)
+      tags.delete(t)
+    end
+
   end
 end
